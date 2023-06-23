f202acaa14f0ae146e03d161443e838c45eb8c4c
maiconcentner <maiconcentner@gmail.com>
2023-06-23 13:27:25 -0300

    Base de dados tratada

--- base_dados/projeto_covid_sp.R
+++ base_dados/projeto_covid_sp.R
@@ -6,8 +6,23 @@ library(ggplot2)
 # set da pasta de trabalho
 setwd("D:\\Mestrado\\1Sem_23\\Ferramentas computacionais\\projeto_final\\base_dados")
 
-#carregando base de dados
+# carregando base de dados
 
 dados_covid_sp <- read.csv('dados_covid_sp.csv', sep = ";")
 base_sp <- read.csv('sp.csv', sep = ";")
-raca_obitos <- read.csv('casos_obitos_raca_cor.csv', sep = ";")
+
+# Tratamento da base de dados
+glimpse(dados_covid_sp)
+dados_covid_sp$datahora <- as.Date(dados_covid_sp$datahora,
+                                  format = '%Y-%m-%d') #convertendo datahora para data
+glimpse(dados_covid_sp)
+
+# excluindo colunas que não iremos usar
+dados_covid_sp_tratado <- select(dados_covid_sp, -c(2,6,9))
+
+# verificando valores vazios
+dados_covid_sp_tratado %>% 
+  sapply(function(x) sum(is.na(x))) # valores NA
+
+dados_covid_sp_tratado %>% 
+  sapply(function(x) sum(is.nan(x))) # valores NAN
