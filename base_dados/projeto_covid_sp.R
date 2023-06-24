cdbdff7ef7f30bbc55587a0e9901e1c4a8c472d7
douglascarrega <douglas.carrega@unesp.br>
2023-06-23 23:45:44 -0300

    alteração de gráficos

--- base_dados/projeto_covid_sp.R
+++ base_dados/projeto_covid_sp.R
@@ -2,10 +2,11 @@
 library(dplyr)
 library(rstatix)
 library(ggplot2)
+library(rcartocolor)
 
 cores <- rcartocolor::carto_pal(12, "Bold")
 # set da pasta de trabalho
-setwd("D:\\Mestrado\\1Sem_23\\Ferramentas computacionais\\projeto_final\\base_dados")
+setwd("C:/Users/W10/aula do banana/trabalho/projeto_ferramentas_final/base_dados")
 
 # carregando base de dados
 
@@ -35,29 +36,47 @@ casos_mes_ano <- dados_covid_sp_tratado %>%
   group_by(datahora) %>% 
   summarise(total_casos = sum(casos))
 
-# plot do gráfico: total de casos acumulado
+# plot do gráfico: total de casos acumulado 
 casos_mes_ano %>% 
-  ggplot(aes(x = datahora, y = total_casos)) +
+  ggplot(aes(x = datahora, y = total_casos * 1e-6)) +
   geom_density(fill = cores[8], stat = "identity", alpha = 0.3) +
   scale_color_manual(values = cores) +
-  theme_bw()+
+  theme ()+
   theme(panel.grid = element_blank(),  # Remove as linhas de grade
         plot.background = element_rect(fill = "white")) +
-  labs(x = "Ano/mês", y = "Total de casos", title = "Total de casos no estado de São Paulo")
+  labs(x = "Ano",
+       y = expression(paste("Total de Casos (", 10^6, ")")),
+       title = "Evolução dos Casos no Estado de São Paulo") +
+  scale_y_continuous(labels = scales::comma, breaks = scales::pretty_breaks(5))
+
+
+
+
 
 # Total de novos casos por dia
 novos_casos_mes_ano <- dados_covid_sp_tratado %>% 
   group_by(datahora) %>% 
   summarise(novos_casos = sum(casos_novos))
 
-novos_casos_mes_ano %>% 
-  ggplot(aes(x = datahora, y = novos_casos)) +
-  geom_density(fill = cores[1], stat = "identity", alpha = 0.3) +
-  scale_color_manual(values = cores) +
-  theme_bw()+
-  theme(panel.grid = element_blank(),  # Remove as linhas de grade
-        plot.background = element_rect(fill = "white")) + 
-  labs(x = "Ano/mês", y = "Total de casos", title = "Novos casos por dia no estado de São Paulo")
 
+'novos_casos_mes_ano %>%
+  ggplot(aes(x = datahora, y = novos_casos *1e-3)) +
+  geom_line(color = cores[1], size = 0.5) +
+  scale_color_manual(values = cores) +
+  theme_minimal() +
+  theme(panel.grid = element_blank(),
+        plot.background = element_rect(fill = "white")) +
+  labs(x = "Ano/Mês",
+       y = "Novos Casos (x 10³)",
+       title = "Novos Casos por Dia no Estado de São Paulo")'
+
+novos_casos_mes_ano %>%
+  mutate(datahora = as.POSIXct(datahora)) %>%
+  ggplot(aes(x = datahora, y = novos_casos *1e-3)) +
+  geom_line(color = "blue") +
+  geom_smooth(method = "loess", se = FALSE, color = "red") +
+  theme_minimal() +
+  labs(x = "Ano/Mês", y = "Novos Casos (x 10³)", title = "Novos Casos por Dia no Estado de São Paulo") +
+  scale_x_datetime(date_labels = "%Y-%m", date_breaks = "6 month")
 
 
