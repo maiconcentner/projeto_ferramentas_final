97578c3eeb6de6700ae6c28ab5d15bb1c8b12068
maiconcentner <maiconcentner@gmail.com>
2023-07-12 12:21:18 -0300

    alterei paleta de cores e organizei os codigos

--- Estudo/covid19.R
+++ Estudo/covid19.R
@@ -9,6 +9,8 @@ library(dplyr)
 library(rio)
 library(readr)
 
+
+
 # carregando base de dados
 
 dados_covid_sp <- read.csv('dados/dados_covid_sp.csv', sep = ";")
@@ -43,57 +45,71 @@ dataset_final_2020 <- left_join(all_mun_sp, soma_casos_2020, by = c("code_muni" 
 min(dataset_final_2020$total_casos_novos)
 max(dataset_final_2020$total_casos_novos)
 
+# Fazendo os mesmos passsos anteriores para os anos de 2021 e 2022
+
+## 2021
+
+  data_inicio_2021 <- as.Date("2021-01-01")
+  data_final_2021 <- as.Date("2021-12-31")
+
+  soma_casos_2021 <- dados_covid_sp %>%
+  filter(datahora >= data_inicio_2021 & datahora <= data_final_2021) %>%
+  group_by(nome_munic, codigo_ibge) %>%
+  summarise(total_casos_novos = sum(casos_novos))
+
+  dataset_final_2021 <- left_join(all_mun_sp, soma_casos_2021, by = c("code_muni" = "codigo_ibge"))
+  min(dataset_final_2021$total_casos_novos)
+  max(dataset_final_2021$total_casos_novos)
+
+
+## 2022
+  data_inicio_2022 <- as.Date("2022-01-01")
+  data_final_2022 <- as.Date("2022-12-31")
+
+  soma_casos_2022 <- dados_covid_sp %>%
+  filter(datahora >= data_inicio_2022 & datahora <= data_final_2022) %>%
+  group_by(nome_munic, codigo_ibge) %>%
+  summarise(total_casos_novos = sum(casos_novos))
+
+  dataset_final_2022 <- left_join(all_mun_sp, soma_casos_2022, by = c("code_muni" = "codigo_ibge"))
+  min(dataset_final_2022$total_casos_novos)
+  max(dataset_final_2022$total_casos_novos)
+
+
+
+## graficos 
+  cores <- rcartocolor::carto_pal(6, "SunsetDark")
+
+## 2020
 ggplot()+
   geom_sf(data = dataset_final_2020, aes(fill = total_casos_novos), color = NA, size = .15)+
   labs(title = "Casos de COVID-19 em 2020 dos Municípios de SP",
        caption = "Fonte: Elaboração própria", size = 8)+
-  scale_fill_distiller(palette = "Paired", limits = c(1, 500000),
+  scale_fill_gradientn(colours = cores, limits = c(1, 600000),
                        name="Total")+
   theme(plot.title = element_text(hjust = 0.5),
         axis.text = element_blank(),
         axis.ticks = element_blank())
 
-# Fazendo os mesmos passsos anteriores para os anos de 2021 e 2022
-
-data_inicio_2021 <- as.Date("2021-01-01")
-data_inicio_2022 <- as.Date("2022-01-01")
-
-data_final_2021 <- as.Date("2021-12-31")
-data_final_2022 <- as.Date("2022-12-31")
-
-soma_casos_2021 <- dados_covid_sp %>%
-  filter(datahora >= data_inicio_2021 & datahora <= data_final_2021) %>%
-  group_by(nome_munic, codigo_ibge) %>%
-  summarise(total_casos_novos = sum(casos_novos))
-
-soma_casos_2022 <- dados_covid_sp %>%
-  filter(datahora >= data_inicio_2022 & datahora <= data_final_2022) %>%
-  group_by(nome_munic, codigo_ibge) %>%
-  summarise(total_casos_novos = sum(casos_novos))
-
-dataset_final_2021 <- left_join(all_mun_sp, soma_casos_2021, by = c("code_muni" = "codigo_ibge"))
-min(dataset_final_2021$total_casos_novos)
-max(dataset_final_2021$total_casos_novos)
-
-dataset_final_2022 <- left_join(all_mun_sp, soma_casos_2022, by = c("code_muni" = "codigo_ibge"))
-min(dataset_final_2022$total_casos_novos)
-max(dataset_final_2022$total_casos_novos)
+## 2021
 
 ggplot()+
   geom_sf(data = dataset_final_2021, aes(fill = total_casos_novos), color = NA, size = .15)+
   labs(title = "Casos de COVID-19 em 2021 dos Municípios de SP",
        caption = "Fonte: Elaboração própria", size = 8)+
-  scale_fill_distiller(palette = "Paired", limits = c(0, 600000),
+  scale_fill_gradientn(colours = cores, limits = c(0, 600000),
                        name="Total")+
   theme(plot.title = element_text(hjust = 0.5),
         axis.text = element_blank(),
         axis.ticks = element_blank())
 
+## 2022
+
 ggplot()+
   geom_sf(data = dataset_final_2022, aes(fill = total_casos_novos), color = NA, size = .15)+
   labs(title = "Casos de COVID-19 em 2022 dos Municípios de SP",
        caption = "Fonte: Elaboração própria", size = 8)+
-  scale_fill_distiller(palette = "Paired", limits = c(0, 200000),
+  scale_fill_gradientn(colours = cores, limits = c(0, 600000),
                        name="Total")+
   theme(plot.title = element_text(hjust = 0.5),
         axis.text = element_blank(),
